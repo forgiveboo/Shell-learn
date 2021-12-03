20d284f52f025dc5a2875d194aa3837790b4bc09
forgiveboo <wyx5051@gmail.com>
2021-12-03 20:59:48 +0800

    add wifiadb.sh

---
+++ wifiadb.sh
@@ -0,0 +1,26 @@
+#/bash/bin
+echo "该脚本用于开启Tasker的WFI ADB权限"
+echo "使用该脚本之前你需要做3件事："
+echo "1. 在Termux中安装android-tools"
+echo "2. 打开手机上的开发者模式和无线调试"
+echo "3. 查看无线调试中的ip和端口以及配对码中的ip和端口并记住"
+echo "输入配对码ip："
+read ip1
+echo "输入配对码port："
+read port1
+adb pair "$ip1:$port1"
+results_pair = `adb pair "$ip1:$port1"`
+if [[$results_pair =~ ""]]
+echo "输入无线调试ip："
+read ip2
+echo "输入无线调试port："
+read port2
+adb connect $ip2:$port2
+adb tcpip 5555
+results_tcpip = `adb tcpip 5555`
+if [[$results_tcpip =~ "5555"]]
+then
+  echo "WIFI ADB已开启"
+else
+  echo "权限开启失败，请检查退出脚本重试"
+fi
