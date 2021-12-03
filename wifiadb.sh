ae74a52198b2fa53645c4afccfb15d15d7a0c7e9
forgiveboo <wyx5051@gmail.com>
2021-12-03 22:05:55 +0800

    update

--- wifiadb.sh
+++ wifiadb.sh
@@ -16,31 +16,29 @@ results_pair = `adb pair "$ip1:$port1"`
 if [[$results_pair =~ "Successfully"]]
 then
   echo "配对完成"
+  echo "输入无线调试ip："
+  read ip2
+  echo "输入无线调试port："
+  read port2
+  adb connect $ip2:$port2
+  results_connect = `adb connect $ip2:$port2`
+  if [[$results_connect =~ "connected"]]
+  then
+    echo"连接成功"
+    adb tcpip 5555
+    results_tcpip = `adb tcpip 5555`
+    if [[$results_tcpip =~ "5555"]]
+    then
+      echo "WIFI ADB已开启"
+    else
+      echo "权限开启失败，请重试"
+      exit 1
+    fi
+  else
+      echo "连接失败，请检查ip以及port是否正确并重试"
+      exit 1
+  fi
 else
   echo "配对失败，请检查ip以及port是否正确并重试"
-  exit 1
-fi
-  
-echo "输入无线调试ip："
-read ip2
-echo "输入无线调试port："
-read port2
-adb connect $ip2:$port2
-results_connect = `adb connect $ip2:$port2`
-if [[$results_connect =~ "connected"]]
-then
-  echo"连接成功"
-else
-  echo "连接失败，请检查ip以及port是否正确并重试"
-  exit 1
-fi
-  
-adb tcpip 5555
-results_tcpip = `adb tcpip 5555`
-if [[$results_tcpip =~ "5555"]]
-then
-  echo "WIFI ADB已开启"
-else
-  echo "权限开启失败，请重试"
   exit 1
 fi
